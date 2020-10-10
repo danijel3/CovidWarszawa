75fd655149fe2416d876edce4e975936ff8aaad9
Danijel Korzinek <danijel@pja.edu.pl>
2020-10-10 19:53:25 +0200

    Update

--- download.py
+++ download.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     d = datetime(2020, 3, 15)
     while d < today:
         fname = f'KORONAWIRUS-komunikat-PPIS-{d.strftime("%Y-%m-%d")}.pdf'
-        fname2 = f'KORONAWIRUS-komunikat-PPIS-{d.strftime("%Y-%m-%d")}.pdf'
+        fname2 = f'KORONAWIRUS-Komunikat-PPIS-{d.strftime("%Y-%m-%d")}.pdf'
         outfile = outdir / fname
         outfile2 = outdir / fname2
         if outfile.exists() or outfile2.exists():
